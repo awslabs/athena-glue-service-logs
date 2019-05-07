f20bdc66c0cfdeb3323e78415f242bfaefcc3990
dcortesi <dcortesi@amazon.com>
2019-05-07 11:11:45 +0100

    Version bump for public release

--- athena_glue_service_logs/version.py
+++ athena_glue_service_logs/version.py
@@ -6,11 +6,11 @@
 
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# or in the "license" file accompanying this file. This file is distributed 
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-# express or implied. See the License for the specific language governing 
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
 """Version number of our project"""
 
-__version__ = 'v5.3.0'
+__version__ = 'v5.3.4'
